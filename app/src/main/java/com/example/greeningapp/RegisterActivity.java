bd803c4a88efd7bcf57afe7cc08b53841a95d64d
yunju22 <parkyoonjoo@naver.com>
2023-08-21 16:51:18 +0900

    회원가입 후 로그인 화면 이동 및 세부 수정

--- app/src/main/java/com/example/greeningapp/RegisterActivity.java
+++ app/src/main/java/com/example/greeningapp/RegisterActivity.java
@@ -4,7 +4,9 @@ package com.example.greeningapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -87,7 +89,16 @@ public class RegisterActivity extends AppCompatActivity {
                             // 회원 정보 데이터베이스에 저장
                             mDatabaseRef.child(firebaseUser.getUid()).setValue(account);
 
-                            Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다.\n3초 뒤 로그인 화면으로 이동합니다", Toast.LENGTH_SHORT).show();
+                            //일정 시간 지나면 다른 액티비티로 이동함
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }, 3000); // 3초 후에 이동
                         } else {
                             Toast.makeText(RegisterActivity.this, "회원가입에 실패하셨습니다.", Toast.LENGTH_SHORT).show();
                         }
